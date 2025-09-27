7e8a2d0e0d723c71feb3aa1d61b19f34b8e0ff31
Christina3131 <christina.mvtch@eduge.ch>
2025-09-27 17:22:10 +0200

    folders

---
+++ rockdance-frontend/rockdancecompany/lib/public/about/about_page.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class AboutPage extends StatelessWidget {
+  const AboutPage({super.key}); // <= const constructor
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      appBar: AppBar(title: Text('About Us')),
+      body: Center(child: Text('About Rock Dance Company')),
+    );
+  }
+}
